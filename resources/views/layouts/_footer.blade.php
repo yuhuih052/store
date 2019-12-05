6ad0e62816d4c49db0ce51e55d233d6a97a8d886
vagrant <vagrant@homestead>
2019-12-05 10:09:10 +0000

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="#" target="_blank">黄宇辉</a> 设计和编码 <span style="color: #e26239;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="yuhuih052@163.com">联系我们</a></p>
+    </div>
+</footer>
