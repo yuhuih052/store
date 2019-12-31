c527edac1f189d5c86ad5b7793e851c7a63cc29a
vagrant <vagrant@homestead>
2019-12-31 07:16:40 +0000

    关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,19 +8,18 @@ use App\Models\UserAddress;
 use App\Models\Order;
 use Carbon\Carbon;
 use App\Exceptions\InsideException;
+use App\Jobs\CloseOrder;
 
 class OrdersController extends Controller
 {
     public function store(OrderRequest $request){
 
         $user = $request->user();
-
         //开启一个数据库事务
         $order = \DB::transaction(function () use ($user, $request){
            $address = UserAddress::find($request->input('address_id'));
            //更新地址最后使用时间
             $address->update(['last_used_at' => Carbon::now()]);
-
             // 创建一个订单
             $order   = new Order([
                 'address'      => [ // 将地址信息放入订单中
@@ -39,7 +38,6 @@ class OrdersController extends Controller
 
             $totalAmount = 0;
             $items       = $request->input('items');
-
             // 遍历用户提交的 SKU
             foreach ($items as $data) {
                 $sku  = ProductSku::find($data['sku_id']);
@@ -60,14 +58,15 @@ class OrdersController extends Controller
 
             // 更新订单总金额
             $order->update(['total_amount' => $totalAmount]);
-
             // 将下单的商品从购物车中移除
             $skuIds = collect($request->input('items'))->pluck('sku_id');
             $user->cartItems()->whereIn('product_sku_id', $skuIds)->delete();
 
             return $order;
-
         });
+
+        $this->dispatch(new CloseOrder($order, config('app.CloseOrderDelay')));
+
         return $order;
 
     }
