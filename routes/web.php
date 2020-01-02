42e2542d58086d301c0cfe691e5b4ca74c2d61b2
vagrant <vagrant@homestead>
2020-01-02 05:28:50 +0000

    引入支付库

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::middleware('auth')->group(function (){
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
         Route::post('orders', 'OrdersController@store')->name('orders.store');
         Route::get('orders', 'OrdersController@index')->name('orders.index');
-        Route::get('orders/{order}','OrderController@details')->name('orders.details');
+        Route::get('orders/{order}','OrdersController@details')->name('orders.details');
     });
 
 });
